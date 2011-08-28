67094e9245c18a661a18097ad4f0221896724cdf
Antti Karttunen <Antti.Karttunen@gmail.com>
2011-08-28 16:38:10 +0300

    first commit

---
+++ testprojects/t_gcd1test/t_gcd1test.brm.scm
@@ -0,0 +1,10 @@
+
+(-MODULE-INFO-BEGIN-)
+;;
+;; t_gcd1test.brm.scm - Output 
+;; Copyright (C) 2010-2011 Antti Karttunen, and subject to GPL v2.
+;;
+(-MODULE-INFO-END-)
+
+(output_n_and_gcd_hi_lo_n_upto_uplimit txd 20'1048575)
+
