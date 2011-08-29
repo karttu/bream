d709d10d4dfcbc4bc97afbe9d41c7a1b8e17f6c5
Antti Karttunen <Antti.Karttunen@gmail.com>
2011-08-29 07:02:40 +0300

    2011-08-29 Morning

---
+++ src/breamlib/Verilog/Board_specific/SPARTAN3-200-SB/uart2defines.v
@@ -0,0 +1,12 @@
+
+// Spartan-6 in our ATLYS is running by default at 100 MHz, 16 cycles per bit,
+// so we divide it with 54, to get 1200 bps:
+
+// 100000000.0/(16*54) = 115740.74074074074
+// which is 1.0046939300411524 * 115200, i.e. only half percent too fast.
+
+// `define UART_BPS_RATE_DIVIDER 12'd54 // For ATLYS running on 100MHz.
+
+// For 50MHz clock we need half smaller divider:
+`define UART_BPS_RATE_DIVIDER 12'd27 // For Spartan-3 running on 50MHz.
+
